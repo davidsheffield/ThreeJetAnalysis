123fced073a3986d22a832191bc040a995e9fbeb
David Sheffield <sheffield@physics.rutgers.edu>
2015-10-22 16:11:04 -0500

    Initialize LogX histogram

--- Utilities/interface/TH1DInitializer.h
+++ Utilities/interface/TH1DInitializer.h
@@ -16,12 +16,13 @@
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 
 #include "TH1D.h"
+#include "TMath.h"
 
 TH1D* TH1DInitializer(edm::Service<TFileService>, const TString, const TString,
 		      const Int_t, const Double_t, const Double_t,
-		      const TString, const TString);
+		      const TString, const TString, const bool doLogX = false);
 TH1D* TH1DInitializer(TFileDirectory*, const TString, const TString,
 		      const Int_t, const Double_t, const Double_t,
-		      const TString, const TString);
+		      const TString, const TString, const bool doLogX = false);
 
 #endif
--- Utilities/src/TH1DInitializer.cc
+++ Utilities/src/TH1DInitializer.cc
@@ -14,9 +14,23 @@
 TH1D* TH1DInitializer(edm::Service<TFileService> fs, const TString name,
 		      const TString title, const Int_t nbinsx,
 		      const Double_t xlow, const Double_t xup,
-		      const TString xaxis_title, const TString yaxis_title)
+		      const TString xaxis_title, const TString yaxis_title,
+                      const bool doLogX)
 {
-    TH1D *histogram = fs->make<TH1D>(name, title, nbinsx, xlow, xup);
+    TH1D *histogram;
+    if (doLogX) {
+        Double_t logxlow = TMath::Log10(xlow);
+        Double_t logxup = TMath::Log10(xup);
+        Double_t binwidth = (logxup - logxlow)/nbinsx;
+        //Double_t xbins[nbinsx+1];
+        Double_t xbins[1001];
+        for (int i=0; i<=nbinsx; ++i) {
+            xbins[i] = TMath::Power(10, logxlow + i*binwidth);
+        }
+        histogram = fs->make<TH1D>(name, title, nbinsx, xbins);
+    } else {
+        histogram = fs->make<TH1D>(name, title, nbinsx, xlow, xup);
+    }
     histogram->GetXaxis()->SetTitle(xaxis_title);
     histogram->GetYaxis()->SetTitle(yaxis_title);
 
@@ -26,9 +40,23 @@ TH1D* TH1DInitializer(edm::Service<TFileService> fs, const TString name,
 TH1D* TH1DInitializer(TFileDirectory *dir, const TString name,
 		      const TString title, const Int_t nbinsx,
 		      const Double_t xlow, const Double_t xup,
-		      const TString xaxis_title, const TString yaxis_title)
+		      const TString xaxis_title, const TString yaxis_title,
+                      const bool doLogX)
 {
-    TH1D *histogram = dir->make<TH1D>(name, title, nbinsx, xlow, xup);
+    TH1D *histogram;
+    if (doLogX) {
+        Double_t logxlow = TMath::Log10(xlow);
+        Double_t logxup = TMath::Log10(xup);
+        Double_t binwidth = (logxup - logxlow)/nbinsx;
+        //Double_t xbins[nbinsx+1];
+        Double_t xbins[1001];
+        for (int i=0; i<=nbinsx; ++i) {
+            xbins[i] = TMath::Power(10, logxlow + i*binwidth);
+        }
+        histogram = dir->make<TH1D>(name, title, nbinsx, xbins);
+    } else {
+        histogram = dir->make<TH1D>(name, title, nbinsx, xlow, xup);
+    }
     histogram->GetXaxis()->SetTitle(xaxis_title);
     histogram->GetYaxis()->SetTitle(yaxis_title);
 
