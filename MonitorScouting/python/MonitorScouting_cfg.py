18ed594910deef93d453139dc441f1ec4fbf7246
David Sheffield <sheffield@physics.rutgers.edu>
2015-10-22 11:28:16 -0500

    Add coommand line options

--- MonitorScouting/python/MonitorScouting_cfg.py
+++ MonitorScouting/python/MonitorScouting_cfg.py
@@ -1,23 +1,37 @@
 import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
 
 process = cms.Process("ScoutingMonitor")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
-process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(10000)
+options = VarParsing.VarParsing('analysis')
+options.outputFile = 'monitoring_histograms.root'
+options.inputFiles = 'file:/uscmst1b_scratch/lpc1/3DayLifetime/dgsheffi/ScoutingPFHT.root'
+options.maxEvents = -1
+options.register('reportEvery',
+                 10000, # default value
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.int,
+                 "Number of events to process before reporting progress.")
+options.parseArguments()
 
-process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(
-        'file:/uscmst1b_scratch/lpc1/3DayLifetime/dgsheffi/ScoutingPFHT.root '
-    )
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(options.maxEvents)
+)
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(
+    options.reportEvery)
+
+process.source = cms.Source(
+    "PoolSource",
+    fileNames = cms.untracked.vstring(options.inputFiles)
 )
 
 process.load('ThreeJetAnalysis.MonitorScouting.monitorscouting_cfi')
 
 process.TFileService = cms.Service(
     "TFileService",
-    fileName = cms.string("monitoring_histograms.root")
+    fileName = cms.string(options.outputFile)
 )
 
 process.p = cms.Path(process.monitorscouting)
